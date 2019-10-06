b372008cb054549edd21fb78521f1aa808e6754b
Paweł Wójtowicz <elendil@konto.pl>
2019-10-06 21:55:19 +0200

    List adjusted - dialog is comming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { ClassesComponent } from './classes/classes.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    MatSidenavModule,
     BrowserAnimationsModule
   ],
   providers: [],
